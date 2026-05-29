0df737d1b7f10681d9c0cdc7444259dd52b10d3b
Maateoeguiazu10 <mateoeguiazu25@gmail.com>
2026-05-29 20:36:58 -0300

    First Commit

---
+++ CineTup.Application/Services/UserServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CineTup.Domain.Entities;
+
+namespace CineTup.Application.Services
+{
+    public class UserServices
+    {
+        public User Get(string name)
+        {
+            return new User() { Name = name };
+        }
+    }
+}
---
+++ CineTup.Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CineTup.Domain.Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
